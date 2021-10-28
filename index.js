448764628a948bedf500006b6518e5df8716446d
Md Arifur Rahman <rahman1193arif@gmail.com>
2021-10-29 02:01:39 +0600

    coonnect to the data base

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const { MongoClient } = require('mongodb');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.l9eit.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+console.log("lol0000000000000",uri);
+
+
+async function run(){
+    try{
+        await client.connect();
+        console.log('connect to the data base')
+    }
+    finally{
+        // await clint.close();
+    }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
